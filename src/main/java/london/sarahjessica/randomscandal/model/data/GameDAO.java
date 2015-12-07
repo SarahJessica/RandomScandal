ecef24f366f0202a2b326dc3b4b5fc466430e45d
Sarah Jessica Leivers <jessica.leivers@bbc.co.uk>
2015-12-07 10:46:38 +0000

    GameDAO, started mongo config

--- src/main/java/london/sarahjessica/randomscandal/model/data/GameDAO.java
+++ src/main/java/london/sarahjessica/randomscandal/model/data/GameDAO.java
@@ -1,5 +1,12 @@
 package london.sarahjessica.randomscandal.model.data;
 
 
-public class GameDA {
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+public class GameDAO {
+
+    // NEED TO CONFIGURE MONGOCLIENT TO PORT 27017
+//    MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+
 }
